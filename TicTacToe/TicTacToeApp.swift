1aa63ceed9eb9d822dda9ff30ca81c81f4141950
Anshul Kanwar <4n5hu1@protonmail.com>
2022-10-27 18:08:24 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Anshul Kanwar on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
